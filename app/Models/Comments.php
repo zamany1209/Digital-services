7d9ce59666c83065cb8cb1920129bf55c3c38e6b
Amir Hossen Zamaniyan <75237110+zamany1209@users.noreply.github.com>
2021-06-30 18:54:05 +0430

    zamany
    Create Model & Migrate (Digital-services)

---
+++ app/Models/Comments.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comments extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'family',
+        'product_id',
+        'user_id',
+        'com_id',
+        'content',
+        'confirmed',
+        'created_at'
+    ];
+}
---
+++ database/migrations/2021_06_30_141953_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('family');
+            $table->string('product_id');
+            $table->string('user_id');
+            $table->string('com_id')->nullable();
+            $table->string('content');
+            // $table->enum('confirmed',['yes','no'])->default('yes');
+            $table->boolean('confirmed')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
